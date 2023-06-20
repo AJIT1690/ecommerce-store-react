934a5323c4aadd5694d64f60b6bd4e39282f0922
ajit1690 <ajit.singh1690@hotmail.com>
2023-06-20 18:46:51 +0530

    basic structure

--- clothing-store/src/App.js
+++ clothing-store/src/App.js
@@ -1,9 +1,43 @@
+import './categories.styles.scss';
 
 
-function App = () => {
+import Directory from './components/directory/directory.component';
+
+
+const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png'
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png'
+    },
+    {
+      id: 3,
+      title: 'Shirts',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png'
+    },
+    {
+      id: 4,
+      title: 'Men',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png'
+    },
+    {
+      id: 5,
+      title: 'Women',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png'
+    }
+
+  ]
+
   return (
     <div>
-      Hello World!!
+      <Directory categories={categories} />
     </div>
   );
 }
